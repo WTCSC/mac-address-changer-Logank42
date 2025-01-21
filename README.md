773e1fbf63b1b36360e25a721fc6cd0f8f253378
Logan-Kirkpatrick <logan_kirkpatrick@icloud.com>
2025-01-21 12:44:43 -0700

    Working changer

--- README.md
+++ README.md
@@ -2,14 +2,6 @@
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=17729447)
 
 
-Main project:
-    -Validate that the adress is vaild
-    -Assigning the new Mac adress
-    -Error messages and explinations on how to fix
-    -Find network interface you want to use to change your Mac adress (ip link show)
-    -README.md with full explonation
-
-
 Extra credit:
     -Figure out linux machine mac adress (ip addr command maybe?)
     -Random Mac adress generator
--- changer.sh
+++ changer.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 new_address() {
-    ip link set dev "$1" down
-    ip link set dev "$1" address "$2"
-    ip link set dev "$1" up
+    ip link set dev "$1" down # Shuts down network interface
+    ip link set dev "$1" address "$2" # Changes the mac adress for the corresponding network interface
+    ip link set dev "$1" up # Turns the network interface on
 }
 
 adress_validation() {
     local mac=$2
 
-    if [[ ! "$mac" =~ ^[0-9A-Fa-f][02468ACEace]$ ]]; then
+    if [[ ! "$mac" =~ ^[0-9A-Fa-f][02468ACEace]$ ]]; then # Checks the first 2 digets to ensure they are even
         echo "Error. First bit of address must be even (2468aceACE)"
         exit 1
-    elif [[ ! "$mac" =~ ^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$ ]]; then
+    elif [[ ! "$mac" =~ ^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$ ]]; then # Checks the whole mac adress making sure it is in the correct format
         echo Error. Mac adress is not formatted correctly. Valid max adress format: "xx:xx:xx:xx:xx:xx"
         exit 1
-    else
+    else # If the first 2 checks pass, it calls the new_adress function
         new_address "$1" "$mac"
     fi
 }
 
 mac=$2
 
-if [ "$1" != "" ]; then
+if [ "$1" != "" ]; then # Checks that there is an argument and then run the adress_validation
     adress_validation "$1" "$mac"
 
-else
+else # Prints an error if there is no arguement
     echo Error. Must have a valid arguement
     exit 1
 fi
