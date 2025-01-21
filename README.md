1c757be09e6b4fd7ed2e1caffbff32363121a5c3
Logan-Kirkpatrick <logan_kirkpatrick@icloud.com>
2025-01-21 14:41:20 -0700

    Final changer

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/tp86o73G)
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=17729447)
 
+# Mac Address Changer
+## Features:
+In this mac address changer, you have a couple of options. You can use your own mac address that you input, or a random one generated. This script does not save your mac address and you cannot recover a previous mac address unless it is saved elsewhere.
+## How to use:
+To use, you must enter the network interface of the mac address you want changed. After you do that, there are two options that you can use.
 
-Extra credit:
-    -Random Mac adress generator
+You can use the -r command, which will generate you a random mac address, or you can enter your own mac address in this format: 'xx:xx:xx:xx:xx:xx'.
+
+An example of a valid input would be 'enp0s3 -r' or 'enp0s3 02:a4:2d:9f:09:cc'. If not entered in this format, you will recive an error message.
+
+Enjoy!
--- changer.sh
+++ changer.sh
@@ -6,14 +6,21 @@ new_address() {
     ip link set dev "$1" up # Turns the network interface on
 }
 
-adress_validation() {
+random_address() {
+    random=$(echo $RANDOM|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/02:\1:\2:\3:\4:\5/')
+    ip link set dev "$1" down
+    ip link set dev "$1" address "$random"
+    ip link set dev "$1" up
+}
+
+address_validation() {
     local mac=$2
 
-    if [[ ! "$mac" =~ ^[0-9A-Fa-f][02468ACEace]$ ]]; then # Checks the first 2 digets to ensure they are even
+    if [[ "$mac" =~ ^[0-9A-Fa-f][02468ACEace]$ ]]; then # Checks the first 2 digets to ensure they are even
         echo "Error. First bit of address must be even (2468aceACE)"
         exit 1
     elif [[ ! "$mac" =~ ^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$ ]]; then # Checks the whole mac adress making sure it is in the correct format
-        echo Error. Mac adress is not formatted correctly. Valid max adress format: "xx:xx:xx:xx:xx:xx"
+        echo Error. Mac address is not formatted correctly. Valid max adress format: "xx:xx:xx:xx:xx:xx"
         exit 1
     else # If the first 2 checks pass, it calls the new_adress function
         new_address "$1" "$mac"
@@ -22,10 +29,14 @@ adress_validation() {
 
 mac=$2
 
-if [ "$1" != "" ]; then # Checks that there is an argument and then run the adress_validation function
-    adress_validation "$1" "$mac"
 
-else # Prints an error if there is no arguement
-    echo Error. Must have a valid arguement
+if [ "$1" = "" ]; then # Checks that there is an argument and prints error message if there is none
+    echo "Error. Must have a valid mac address or -r command"
     exit 1
+
+elif [ "$mac" = "-r" ]; then
+    random_address "$1" "$mac"
+
+else
+    address_validation "$1" "$mac"
 fi
